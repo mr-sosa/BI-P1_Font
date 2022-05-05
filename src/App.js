c14ea6e48baf3c0f44cd3ad48980602bf091269c
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-05-05 11:39:23 -0500

    page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Form, Row, Col, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  const urlFast = '';
+  const urlStrong = '';
+
+  const [study, setStudy] = useState('');
+  const [condition, setCondition] = useState('');
+
+  const fastCheck = () => {
+
+  };
+
+  const strongCheck = () => {
+
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form>
+          <Row className="align-items-center">
+            <Col xs="auto">
+              <Form.Label htmlFor="inlineFormInput" visuallyHidden>
+                Name
+              </Form.Label>
+              <Form.Control
+                className="mb-2"
+                id="inlineFormInput"
+                placeholder="Study"
+                value={study}
+                
+              />
+            </Col>
+            <Col xs="auto">
+              <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
+                Username
+              </Form.Label>
+              <FormControl 
+                className="mb-2" 
+                id="inlineFormInputGroup" 
+                placeholder="Condition" 
+                value={condition}
+                
+              />
+            </Col>
+            <Col xs="auto">
+              <Button type="submit" className="mb-2" onClick={fastCheck()}>
+                Fast Check
+              </Button>
+            </Col>
+            <Col xs="auto">
+              <Button type="submit" className="mb-2" onClick={strongCheck()}>
+                Strong check
+              </Button>
+            </Col>
+          </Row>
+        </Form>
       </header>
     </div>
   );
