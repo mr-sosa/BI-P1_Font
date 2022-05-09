17896951b0c5e4bdb5adc63f6465d69fd1547c7a
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-05-09 13:49:47 -0500

    cambios interfaz y mensaje de respuesta

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Row, Col, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
@@ -6,58 +6,120 @@ function App() {
   const urlFast = '';
   const urlStrong = '';
 
-  const [study, setStudy] = useState('');
-  const [condition, setCondition] = useState('');
+  const [datos, setDatos] = useState({
+    study: '',
+    condition: ''
+  })
 
-  const fastCheck = () => {
+  const handleInputChange = (event) => {
+    setDatos({
+        ...datos,
+        [event.target.name] : event.target.value
+    })
+  }
 
+  const [resp, setResp] = useState('');
+
+  const fastCheck = async () => {
+    var dataSend = {
+      'study_and_condition': datos.study + ' . ' + datos.condition
+    };
+
+    const response = await fetch(urlFast, {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataSend),
+    }).then((respu) => respu.json())
+    .then(() => {
+      const exactitud = (response.exactitud) * 100;
+      setResp(exactitud.toString + '%')
+    });
   };
 
-  const strongCheck = () => {
+  const strongCheck = async () => {
+    var dataSend = {
+      'study_and_condition': datos.study + ' . ' + datos.condition
+    };
 
+    const response = await fetch(urlStrong, {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataSend),
+    }).then((respu) => respu.json())    
+    .then(() => {
+      const exactitud = (response.exactitud) * 100;
+      setResp(exactitud.toString + '%')
+    });
   };
+
+  
 
   return (
     <div className="App">
       <header className="App-header">
+        <div className='tittle'>
+          <h1>Welcome doctor</h1>
+          <h3>Instructions:</h3>
+          <div className='instructions'>
+            <ol>
+              <li>Put a study.</li>
+              <li>Put a condition.</li>
+              <li>Click one of the two buttons (Fast Check or Strong Check).</li>
+            </ol>
+            <p>Fast check will use a ...</p>
+            <p>Strong check will use a ...</p>
+          </div>
+        </div>
         <Form>
           <Row className="align-items-center">
             <Col xs="auto">
               <Form.Label htmlFor="inlineFormInput" visuallyHidden>
                 Name
               </Form.Label>
-              <Form.Control
-                className="mb-2"
+              <input 
+                type="text" 
+                placeholder="Study"  
+                className="form-control mb-2" 
                 id="inlineFormInput"
-                placeholder="Study"
-                value={study}
-                
+                onChange={handleInputChange} 
+                name="study"
               />
             </Col>
             <Col xs="auto">
               <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
                 Username
               </Form.Label>
-              <FormControl 
-                className="mb-2" 
-                id="inlineFormInputGroup" 
-                placeholder="Condition" 
-                value={condition}
-                
+              <input 
+                type="text" 
+                placeholder="Condition"  
+                className="form-control mb-2" 
+                id="inlineFormInput"
+                onChange={handleInputChange} 
+                name="condition"
               />
             </Col>
             <Col xs="auto">
-              <Button type="submit" className="mb-2" onClick={fastCheck()}>
+              <Button className="mb-2" onClick={ ()=>{setResp('fastCheck'+datos.study);fastCheck();}}>
                 Fast Check
               </Button>
             </Col>
             <Col xs="auto">
-              <Button type="submit" className="mb-2" onClick={strongCheck()}>
+              <Button className="mb-2" onClick={()=>{setResp('strongCheck'+datos.condition);strongCheck();}}>
                 Strong check
               </Button>
             </Col>
           </Row>
         </Form>
+        <div className='resp'>
+          <h3>Response</h3>
+          <pre id='resp-data'>{resp}</pre>
+        </div>        
       </header>
     </div>
   );
